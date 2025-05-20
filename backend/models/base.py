9ba27eaae1ff451875d34aafcd50f96f030eb13e
filip-herceg <pvt.filip.herceg@gmail.com>
2025-05-20 18:13:05 +0000

    Format updated_at column for better readability

--- backend/models/base.py
+++ backend/models/base.py
@@ -9,4 +9,6 @@ class BaseModel(Base):  # type: ignore[misc, valid-type]
     __abstract__ = True
     id = mapped_column(Integer, primary_key=True, autoincrement=True)
     created_at = mapped_column(DateTime, default=func.now(), nullable=False)
-    updated_at = mapped_column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+    updated_at = mapped_column(
+        DateTime, default=func.now(), onupdate=func.now(), nullable=False
+    )
